f0944a3e8e0969c99d239dc45ba9e3c8ce31039e
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-09-27 18:40:36 +0200

    Added euro jackpot

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const wynik = [];
+const wynik2 = [];
 const numbers = document.querySelector(".numbers");
+const numbersEuro = document.querySelector(".numbersEuro");
+const numbersEuro2 = document.querySelector(".numbersEuro2");
 
 const losowanie = () => {
   resetBtn.classList.remove("active");
@@ -21,12 +24,42 @@ const losowanie = () => {
   div.innerHTML = arr.map((num) => `<span>${num}</span>`).join("");
   numbers.appendChild(div);
 };
+const losowanieEuro = () => {
+  resetBtn.classList.remove("active");
+  const div = document.createElement("div");
+  const div2 = document.createElement("div");
+  div.classList.add("spanel");
+  div.classList.add("euro");
+  div2.classList.add("spanel");
+  div2.classList.add("euro");
+  var arrA = [];
+  var arrB = [];
+  while (arrA.length < 5) {
+    var r = Math.floor(Math.random() * 50) + 1;
+    if (arrA.indexOf(r) === -1) arrA.push(r);
+  }
+  while (arrB.length < 2) {
+    var r = Math.floor(Math.random() * 12) + 1;
+    if (arrB.indexOf(r) === -1) arrB.push(r);
+  }
+  // var arr = [...arrA, ...arrB];
+
+  wynik.push(arrA);
+  wynik2.push(arrB);
+
+  div.innerHTML = arrA.map((num) => `<span>${num}</span>`).join("");
+  div2.innerHTML = arrB.map((num) => `<span>${num}</span>`).join("");
+  numbersEuro.appendChild(div);
+  numbersEuro2.appendChild(div2);
+};
 
 const resetHandler = function () {
   location.reload();
 };
 
 const btnLos = document.querySelector(".btn-los");
+const btnLosEuro = document.querySelector(".btn-los-euro");
 const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", resetHandler);
 btnLos.addEventListener("click", losowanie);
+btnLosEuro.addEventListener("click", losowanieEuro);
